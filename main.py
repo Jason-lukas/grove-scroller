77f86c5f26817b155b8e7c7b5ee069180d0080d3
Unknown <yacenty67@gmail.com>
2018-04-02 13:25:47 +0200

    Added main

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+def main():
+    # input
+    # sim
+        # move
+        # coll
+    # draw
+    pass
+
+
+main()
