05db4efbfbf68f71a26b29e8cfa0659fd3102236
Unknown <yacenty67@gmail.com>
2018-04-02 13:34:26 +0200

    Opening window

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+import config
+
 
 def main():
-    # input
-    # sim
-        # move
-        # coll
-    # draw
-    pass
+    pygame.init()
+    pygame.display.set_mode(config.resolution, DOUBLEBUF|OPENGL)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+                    
+        # sim
+            # move
+            # coll
+        # draw
 
 
 main()
