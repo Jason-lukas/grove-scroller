52fe79719148badb19ceaa31b8c43675aaedaa84
Marek <marqus34@gmail.com>
2018-04-02 18:06:07 +0200

    Asteroid random rotation

--- asteroid.py
+++ asteroid.py
@@ -6,8 +6,10 @@ from utility import *
 
 
 class Asteroid(Entity):
-    def __init__(self, rot_rate=60.0):
-        super(Asteroid, self).__init__()
+    def __init__(self, pos, rot_rate=60.0):
+        super(Asteroid, self).__init__(pos)
+        self.color_f = (0.5, 0.5, 0.5)
+
         self.rot_speed = self.rot_random(rot_rate)
         self.speed = (-1, 0, 0)
 
@@ -16,7 +18,11 @@ class Asteroid(Entity):
         return rate, uniform(-1, 1), uniform(-1, 1), uniform(-1, 1)
 
     def sim(self):
-        super(Asteroid, self).sim(self)
+        super(Asteroid, self).sim()
 
-        move = (s * config.delta_t for s in self.speed)
+        move = tuple(s * config.delta_t for s in self.speed)
         self.pos = add(self.pos, move)
+
+        # simplified rotation
+        angle = self.rot_speed[0] * config.delta_t
+        self.rot = (self.rot[0] + angle, self.rot_speed[1], self.rot_speed[2], self.rot_speed[3])
--- main.py
+++ main.py
@@ -8,20 +8,30 @@ import config
 from utility import *
 from entity import Entity
 from my_input import *
+from asteroid import Asteroid
 
 
 def main():
+
+    # Initial setup:
     pygame.init()
     pygame.display.set_mode(config.resolution, DOUBLEBUF | OPENGL)
     gluPerspective(config.camera_fov, config.resolution[0] / config.resolution[1], 0.1, 50.0)
-
     glTranslatef(0, 0, config.camera_z)
 
+    # Entities
+    simulated = list()
+    drawn = list()
+
     test_cube = Entity((0, 0, 0))
-
     move = 0.5
+    drawn.append(test_cube)
 
-    wait_t= int(config.delta_t*1000)
+    asteroid = Asteroid((5, 0, 0))
+    simulated.append(asteroid)
+    drawn.append(asteroid)
+
+    wait_t = int(config.delta_t*1000)
 
     while True:
         for event in pygame.event.get():
@@ -44,11 +54,15 @@ def main():
 
                 print("x: {} y: {} z: {}".format(test_cube.pos[0], test_cube.pos[1], test_cube.pos[2]))
 
+        for entity in simulated:
+            entity.sim()
+
         mouse_pos = get_input()
         test_cube.pos = (mouse_pos[0], mouse_pos[1], 0)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        test_cube.draw()
+        for entity in drawn:
+            entity.draw()
         pygame.display.flip()
         pygame.time.wait(wait_t)
 
