979e2de687d2ae85925db81f6458e235b79a8365
Unknown <yacenty67@gmail.com>
2018-04-06 10:54:19 +0200

    Fixed bug

--- ship.py
+++ ship.py
@@ -10,5 +10,5 @@ target_y = my_input.mouse_y
 class Ship(Entity):
     def sim(self):
         move = config.max_speed * config.delta_t
-        y = utility.clamp(target_y, pos[1] - move, pos[1] + move)
-        pos = (pos[0], y, pos[2])
+        y = utility.clamp(target_y, self.pos[1] - move, self.pos[1] + move)
+        pos = (self.pos[0], y, self.pos[2])
